224ff2849a982479bdd9aaf97b95338173c5fda4
ivan <ivan.garcia@enti.cat>
2025-02-21 12:23:28 +0100

    tablero

--- AA2_water_color_sort_Eloi_Ivan/AA2_water_color_sort_Eloi_Ivan/main.cpp
+++ AA2_water_color_sort_Eloi_Ivan/AA2_water_color_sort_Eloi_Ivan/main.cpp
@@ -10,16 +10,22 @@ int main() {
 
 	for (int i = 0; i < ALTO; i++) {
 		for (int j = 0; j < ANCHO; j++) {
-			tablero[i][j] = '-';
+			tablero[i][j] = ' ';
 		}
 	}
 
+
+	std::cout << "\\ / \\ / \\ / \\ / \\ / \\ /" << std::endl;
 	for (int i = 0; i < ALTO; i++) {
 		for (int j = 0; j < ANCHO; j++) {
+			std::cout << "|";
 			std::cout << tablero[i][j];
+			std::cout << "| ";
 		}
 		std::cout << std::endl;
 	}
+	std::cout << "''' ''' ''' ''' ''' '''" << std::endl;
+	std::cout << " 1   2   3   4   5   6 " << std::endl;
 
 	return 0;
 }
